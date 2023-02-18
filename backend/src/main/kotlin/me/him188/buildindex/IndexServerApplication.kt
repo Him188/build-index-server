35bc5f8b44563a2456ee46917271050cfdfbaf80
Him188 <Him188@mamoe.net>
2023-02-18 13:32:01 +0000

    cmd

--- backend/src/main/kotlin/me/him188/buildindex/IndexServerApplication.kt
+++ backend/src/main/kotlin/me/him188/buildindex/IndexServerApplication.kt
@@ -39,10 +39,11 @@ object IndexServerApplication {
         val parser = ArgParser("IndexServer", useDefaultHelpShortName = false)
 
         val host by parser.option(ArgType.String, "host", "h", "Listen hostname").default("0.0.0.0")
-        val port by parser.option(ArgType.Int, "port", "p", "Port number").default(5939)
+        val port by parser.option(ArgType.Int, "port", "p", "Port number").default(5932)
 
         val workingDir by parser.option(ArgType.String, "dir", "wd", "Working directory")
             .default(System.getProperty("user.dir"))
+        parser.parse(args)
 
 //        val dbUrl by parser.option(ArgType.String, "db", description = "Database connection url").required()
 //        val dbDriver by parser.option(ArgType.String, "dbdriver", description = "Database driver").required()
@@ -51,7 +52,6 @@ object IndexServerApplication {
 
         println("Starting Build Index Server")
         File(workingDir).mkdirs()
-        parser.parse(args)
         start(workingDir, port, host)
     }
 
@@ -104,11 +104,100 @@ object IndexServerApplication {
             }
         }
 
+//        println(composeSinglePermission { 
+//            module("mirai-core").branch("dev").index()
+//        })
+
+//        transaction(db) {
+////            Users.deleteWhere { username eq "mirai-github-actions" }
+//            Users.insert {
+//                it[username] = "mirai-github-actions"
+//                it[password] = Random.nextBytes(16).also {
+//                    println(it.toUHexString())
+//                }
+//            }
+//        }
+
         server.start(true)
         appScope.cancel()
     }
 
     private fun registerCommands(db: Database, commandManager: CommandManager) {
+        commandManager.register(Command("add-user", "Add a user") { (username, password) ->
+            val existing = runTransaction(db) { Users.select { Users.username eq username }.singleOrNull()?.toUser() }
+            if (existing != null) {
+                throw IllegalArgumentException("User '$username' already exist.")
+            }
+
+            val count = runTransaction(db) {
+                Users.insert {
+                    it[Users.username] = username
+                    it[Users.password] = UserPasswordEncryption.encrypt(password).value
+                }.insertedCount
+            }
+
+            if (count == 1) {
+                echo("Successfully added user '$username'.")
+            } else {
+                echo("Operation failed.")
+            }
+        })
+
+        commandManager.register(Command("set-password", "Set a user's password") { (username, password) ->
+            runTransaction(db) { Users.select { Users.username eq username }.singleOrNull()?.toUser() }
+                ?: throw IllegalArgumentException("User '$username' not found.")
+
+            val count = runTransaction(db) {
+                Users.update({ Users.username eq username }) {
+                    it[Users.password] = UserPasswordEncryption.encrypt(password).value
+                }
+            }
+
+            if (count == 1) {
+                echo("Successfully set.")
+            } else {
+                echo("Operation failed.")
+            }
+        })
+
+        commandManager.register(Command("remove-user", "Add a user") { (username) ->
+            val count = runTransaction(db) {
+                Users.deleteWhere { Users.username eq username }
+            }
+
+            if (count == 1) {
+                echo("Successfully deleted user '$username'.")
+            } else {
+                echo("Operation failed.")
+            }
+        })
+
+        commandManager.register(Command("list-users", "List users") { _ ->
+            val list = runTransaction(db) { Users.selectAll().map { it.toUser() } }
+
+            echo("Total ${list.size} users: ")
+            list.forEachIndexed { index, user ->
+                echo("${index + 1}. ${user.username}")
+            }
+        })
+
+        data class UserPermission(
+            val username: String,
+            val permission: String,
+        )
+
+        commandManager.register(Command("list-permissions", "List permissions") { _ ->
+            val list = runTransaction(db) {
+                UserPermissions.crossJoin(Users).selectAll()
+                    .map { UserPermission(it[Users.username], it[UserPermissions.permission]) }
+            }
+
+            echo("Total ${list.size} permissions: ")
+            list.forEachIndexed { index, perm ->
+                echo("${index + 1}. ${perm.username}: ${perm.permission}")
+            }
+        })
+
         commandManager.register(Command("grant", "Grant permission") { (username, perm) ->
             val user = runTransaction(db) { Users.select { Users.username eq username }.singleOrNull()?.toUser() }
                 ?: throw IllegalArgumentException("User '$username' not found.")
@@ -159,4 +248,26 @@ fun Application.indexServerApplicationModule(db: Database) {
         })
     }
     configureRouting(db)
+}
+
+
+private fun ByteArray.toUHexString(
+    separator: String = " ",
+    offset: Int = 0,
+    length: Int = this.size - offset
+): String {
+    if (length == 0) {
+        return ""
+    }
+    val lastIndex = offset + length
+    return buildString(length * 2) {
+        this@toUHexString.forEachIndexed { index, it ->
+            if (index in offset until lastIndex) {
+                val ret = it.toUByte().toString(16).uppercase()
+                if (ret.length == 1) append('0')
+                append(ret)
+                if (index < lastIndex - 1) append(separator)
+            }
+        }
+    }
 }
